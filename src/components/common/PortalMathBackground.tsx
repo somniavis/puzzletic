3e4ddcd0358da6c1e1710258b4cbd9da3bcf2944
somniavis <fitnylab@gmail.com>
2026-04-22 23:08:57 +0900

    Restore purple game background

--- src/components/common/PortalMathBackground.tsx
+++ src/components/common/PortalMathBackground.tsx
@@ -9,14 +9,14 @@ export const PortalMathBackground: React.FC = () => (
             <div className="portal-math-background__math portal-math-background__math--a">+</div>
             <div className="portal-math-background__math portal-math-background__math--b">-</div>
             <div className="portal-math-background__math portal-math-background__math--c">x</div>
-            <div className="portal-math-background__math portal-math-background__math--d">/</div>
+            <div className="portal-math-background__math portal-math-background__math--d">÷</div>
             <div className="portal-math-background__math portal-math-background__math--e">+</div>
             <div className="portal-math-background__math portal-math-background__math--f">x</div>
-            <div className="portal-math-background__math portal-math-background__math--g">/</div>
+            <div className="portal-math-background__math portal-math-background__math--g">÷</div>
             <div className="portal-math-background__math portal-math-background__math--h">-</div>
             <div className="portal-math-background__math portal-math-background__math--i">+</div>
             <div className="portal-math-background__math portal-math-background__math--j">x</div>
-            <div className="portal-math-background__math portal-math-background__math--k">/</div>
+            <div className="portal-math-background__math portal-math-background__math--k">÷</div>
             <div className="portal-math-background__math portal-math-background__math--l">-</div>
         </div>
     </div>
